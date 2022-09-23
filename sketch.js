948180e4700283d057e0f74471331c6c24482205
kenjife <85642106+kenjife@users.noreply.github.com>
2022-09-23 09:06:56 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,26 @@
+let zBereich;
+
 function setup() {
-  createCanvas(400, 400);
+  zBereich = createCanvas(windowWidth, windowHeight);
+  zBereich.position(0,0); // anfang canvas links oben
+  zBereich.style('z-index','-1'); // verschieben auf z achse
 }
 
 function draw() {
-  background(220);
+  // ohne background transparent
+  // background(123, 112, 204);
+  noStroke;
+  if (mouseX > windowWidth/2 && mouseY > windowHeight/2){
+    fill(123, 112, 204, 50);
+  }
+  if (mouseX > windowWidth/2 && mouseY < windowHeight/2){
+    fill(204, 112, 146, 50);
+  }
+  if (mouseX < windowWidth/2 && mouseY < windowHeight/2){
+    fill(204, 183, 112, 50);
+  }
+  if (mouseX < windowWidth/2 && mouseY > windowHeight/2){
+    fill(112, 204, 132, 50);
+  }
+  circle(mouseX, mouseY, 20);
 }
